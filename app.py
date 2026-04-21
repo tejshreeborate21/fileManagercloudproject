855064b6b926709b56027b6075f101ec8f7953d6
tejshreeborate21 <tejshreeborate@gmail.com>
2026-04-21 10:34:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def delete_file(filename):
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
